29cea2b6775fc2700190bbbc72ed4be5ef298be5
MatteoCiardi <ciardimatteo99@gmail.com>
2025-09-06 17:22:19 +0200

    scritta la logica js per il calcolo

--- js/script.js
+++ js/script.js
@@ -1,8 +1,40 @@
-const userData = document.querySelector("input-data");
-const userKm = document.querySelector("input-km");
-const userAge = document.querySelector("select-age");
-const priceForKm = 0.21;
+// ELEMENTI DI INPUT
+let ageField = document.getElementById("age");
+let kmField = document.getElementById("kilometers");
+const button = document.getElementById("my_Button");
+const form = document.getElementById("my_Form");
 
-generate.addEventListener("submit", calculateCostTicket);
+form.addEventListener("submit", calculateTicketCost);
 
-// FUNZIONE PER CALCOLARE IL COSTO
+// ELEMENTI DI OUTPUT
+const userAge = document.getElementById("user_Age");
+const userKm = document.getElementById("user_Km");
+const ticketPrice = document.getElementById("user_TicketCost");
+
+// FUNZIONE PER CALCOLARE IL COSTO DEL BIGLIETTO
+function calculateTicketCost(event) {
+    event.preventDefault();
+
+    const ageValue = Number(ageField.value.trim());
+    const kmValue = Number(kmField.value.trim());
+    const normalTicketPrice = (kmValue * 0.21);
+    let finalPrice;
+
+    if (ageValue < 18) {
+        finalPrice = normalTicketPrice * 0.8;
+
+    } else if (ageValue > 65) {
+        finalPrice = normalTicketPrice * 0.6;
+
+    } else {
+        finalPrice = normalTicketPrice;
+
+    }
+
+    finalPrice = finalPrice.toFixed(2);
+
+    userAge.innerText = ageValue;
+    userKm.innerText = kmValue;
+    ticketPrice.innerText = finalPrice;
+}
+
